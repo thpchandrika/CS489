dfdc31c6fdf5e32340401fd47712e10b735a26f9
thpchandrika <thpchandrika@gmail.com>
2024-04-18 17:36:06 -0500

    lab10 part 2 added

---
+++ Lab7/ads/src/test/java/chandrikathapa/com/ads/AdsApplicationTests.java
@@ -0,0 +1,13 @@
+package chandrikathapa.com.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
